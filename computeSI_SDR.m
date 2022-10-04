3c39799cecace723fb30e1408ec50e661efe7cca
stevencrawford-meta <stevencrawford@fb.com>
2022-10-04 09:59:35 -0700

    Update computeSI_SDR.m
    cleaner code, simpler computation

--- computeSI_SDR.m
+++ computeSI_SDR.m
@@ -3,15 +3,15 @@
 % Reference: SDR – HALF-BAKED OR WELL DONE? by Le Roux et. al
 % https://www.jonathanleroux.org/pdf/LeRoux2019ICASSP05sdr.pdf
 
-function si_sdr = computeSI_SDR(cleanReference,processedEstimate)
+function si_sdr = computeSI_SDR(cleanRef,processedEst)
     % compute reference signal energy 
-    refSigEnergy = sum(cleanReference .^ 2);
+    ref_energy = sum(cleanRef .^ 2);
     % compute the optimal scaling factor alpha 
-    alpha = sum(cleanReference .* processedEstimate) / refSigEnergy;
+    alpha = dot(processedEst',cleanRef) / ref_energy;
     % compute the orthogonal projection of the estimate
-    e_target = alpha * cleanReference;
+    e_tar = alpha * cleanRef;
     % compute the residual distortion 
-    e_res = processedEstimate - e_target;
+    e_res = processedEst - e_tar;
     % compute SI-SDR
-    si_sdr = 10*log10((sum(e_target .^ 2)) / (sum(e_res .^ 2)));
+    si_sdr = 10*log10(norm(e_tar)^2 / norm(e_res)^2);
 end
